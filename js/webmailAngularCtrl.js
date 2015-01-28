b90839290dbd753652f5eb639c1063716b5d2ce5
ellie.yang <yangxi@server.fake>
2015-01-28 12:10:20 +0800

    Organized structure

--- js/webmailAngularCtrl.js
+++
@@ -1,8 +0,0 @@
-webmaily.controller("webmailController",function($scope){
-    $scope.master = {firstName:"Xi", email:"easymail@gmail.com"};
-    $scope.reset = function() {
-        $scope.user = angular.copy($scope.master);
-    };
-    //invokes the reset() method
-    $scope.reset();
-});
