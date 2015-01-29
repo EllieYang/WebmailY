198d6ef4ffa3e9c58ecfaca7e98829817b4d257a
ellie.yang <yangxi@server.fake>
2015-01-29 18:43:31 +0800

    Update is never happening.....SO SAD!!!!!!

--- js/controller/mailCtrl.js
+++ js/controller/mailCtrl.js
@@ -1,4 +1,41 @@
 webmaily.controller("mailController",function($scope){
+    
     $scope.labelId = "Label_2";
+    $scope.labels = [];
+    $scope.allThreads = [];
+    $scope.userSpaces=[];
+    
+    angular.element(window).bind('load', function() {
+        handleClientLoad();
+    });
+    
+    $scope.$watchCollection('labels', function (newVal, oldVal) {
+        if (newVal) {
+            showLabels($scope.labels);
+        }
+    });
+    $scope.$watchCollection('allThreads', function (newVal, oldVal) {
+        if (newVal) {
+            $scope.labels.forEach(function(label){
+                //getThreads(label,$scope.allThreads);
+                var userSpace = {};
+                userSpace.label = label;
+                
+                
+                    $scope.allThreads.forEach(function(thread){
+                        threadsFetcher.fetch(label,thread);
+                    });
+                    userSpace.threads = threadsFetcher.threadArray;
+                    $scope.userSpaces.push(userSpace);
+                 
+                safeApply($scope,function(){});
+            });
+        }
+        
+    });
     //$scope.googleApi = jQuery.data($(document),'googleApi');
+    function safeApply(scope, fn) {
+        (scope.$$phase || scope.$root.$$phase) ? fn() : scope.$apply(fn);
+    }
+    
 });
--- js/lib/webmailGapi.js
+++ js/lib/webmailGapi.js
@@ -3,6 +3,7 @@
   var clientId = '130554426228-5n2t4fcm2k9g977mvodfh9vo9591u69t.apps.googleusercontent.com';
   var apiKey = 'AIzaSyCeE7WUuVzyOQUlQuRuSZ5O_h_cw4MLn2k';
   var scopes = 'https://www.googleapis.com/auth/gmail.readonly';
+  
   // Use a button to handle authentication the first time.
   function handleClientLoad() {
     gapi.client.setApiKey(apiKey);
@@ -17,7 +18,6 @@
     var authorizeButton = document.getElementById('authorize-button');
     if (authResult && !authResult.error) {
       authorizeButton.style.visibility = 'hidden';
-      change("googleApi","lalalalalla");
       makeApiCall();
     } else {
       authorizeButton.style.visibility = '';
@@ -60,89 +60,68 @@
         }); 
           
         userLabelReq.execute(function(resp){
-            var labels = resp.labels;
-            showLabels(labels);
-        });
-
-          
-        //Get Email Threads
-        var request = gapi.client.gmail.users.threads.list({
-            'userId':'me'
-        });
-        request.execute(function(resp){
-          
-            resp.threads.forEach(function(thread){
-                var messageDiv = document.createElement('div');
-                messageDiv.className = 'messageDiv';
-                var headerDiv = document.createElement('div');
-                headerDiv.className = 'headerDiv';
-                var snippetDiv = document.createElement('p');
-                snippetDiv.className = 'snippetDiv';
-                var senderDiv = document.createElement('p');
-                var subjectDiv = document.createElement('p');
-                          
-                var emailThreads = gapi.client.gmail.users.threads.get({'userId':'me','id':thread.id});
-                emailThreads.execute(function(content){
-                    var lastMessage = content.messages[(content.messages.length-1)];
-                    
-                    //Get label of this thread
-                    for (var index = lastMessage.labelIds.length-1;index>-1;index--){
-                        //Loop from the end because customized label comes last
-                        if (lastMessage.labelIds[index]==angular.element($("#controllerTag")).scope().labelId){
-                            console.log(lastMessage.labelIds[index]);
-                            //Show snippet of this thread, i.e. snippet of the last message in this thread
-                            if (lastMessage.snippet){
-                                snippetDiv.innerHTML += lastMessage.snippet + '...';   
-                            }else{
-                                snippetDiv.innerHTML += 'This message has no content';  
-                            }
-                            //Show header information related to this thread
-                            if(lastMessage.payload == null){
-                                console.log("payload null");
-                            }else {
-                                lastMessage.payload.headers.forEach(function(header){
-                                    if(header.name == "Subject"){
-                                       // console.log(header.value);
-                                        subjectDiv.innerHTML += '<label>Subject:</label> '+ header.value;
-                                    }
-                                }); 
-                                lastMessage.payload.headers.forEach(function(header){
-                                    if(header.name == "From"){
-                                        //console.log(header.value);
-                                        senderDiv.innerHTML += '<label>From:</label> '+ header.value;
-                                    }
-                                });
-                            }
-                        }
-                    }
-                    
-                });
-                messageDiv.appendChild(headerDiv);
-                messageDiv.appendChild(snippetDiv);
-                headerDiv.appendChild(senderDiv);
-                headerDiv.appendChild(subjectDiv);
-               
-                document.getElementById("content").appendChild(messageDiv);
+            var labels = [];
+            resp.labels.forEach(function(label){
+                if(label.type == 'user'){
+                    labels.push(label);
+                }
             });
+            change("labels",labels);
+            getAllThreads();
         });
-      });
-  }
-function showLabels(labels){
-    labels.forEach(function(label){
-        if(label.type == 'user'){
-            var labelDiv = document.createElement('span');
-            labelDiv.className = 'labelDiv';
-            labelDiv.innerHTML += label.name;
-            document.getElementById('labelContainer').appendChild(labelDiv);
-        }
+  });
+}
+
+var threadsFetcher = {
+    
+    threadArray: [],
+    
+    fetch: function (label,thread){
+
+        gapi.client.gmail.users.threads.get({'userId':'me','id':thread.id}).then(function(response){
+            
+            var content = response.result;
+            var lastMsg = content.messages[(content.messages.length-1)];
+            if(lastMsg.labelIds[lastMsg.labelIds.length-1] == label.id){
+                    var pushedThread = {"thread":thread};
+                    pushedThread.lastMsg = {"msg":lastMsg,"header":{},"snippet":''};
+                    pushedThread.lastMsg.snippet = lastMsg.snippet ? lastMsg.snippet : 'This message has no content';
+                    if(lastMsg.payload){
+                        lastMsg.payload.headers.forEach(function(header){
+                            if(header.name == "Subject"){
+                                pushedThread.lastMsg.header.subject = header.value;
+                            }else if(header.name == "From"){
+                                pushedThread.lastMsg.header.sender = header.value;
+                            }
+                        }); 
+                    }else{
+                        console.log("payload null");
+                    }
+                    this.threadArray.push(pushedThread);
+                }
+        },function(){
+            console.log("Something went wrong");
+        },this);
+
+    }
+};
+
+function getAllThreads(){
+    
+    var req1 = gapi.client.gmail.users.threads.list({
+        'userId':'me'
+     });
+    req1.then(function(resp){
+        change("allThreads",resp.result.threads);
     });
+}
+
+function showLabels(labels){
+    
     labels.forEach(function(label){
         
-        if (label.id == 'INBOX'){
-            var labelDiv = document.createElement('div');
-            labelDiv.className = 'labelDivINBOX';
-            labelDiv.innerHTML += 'Show all messages in inbox';
-            document.getElementById('labelContainer').appendChild(labelDiv);
-        }
+        var userSpace = {};
+        userSpace.label = label;
+        //userSpace.threads = getThreads(label);
     });
 }
